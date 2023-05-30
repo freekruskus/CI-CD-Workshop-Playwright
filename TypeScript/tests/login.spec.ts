7fe415f96ed7da9491288506f68e23632f882abb
Michel Lalmohamed <michellalmohamed@valori.nl>
2023-05-30 14:17:59 +0200

    code setp

---
+++ TypeScript/tests/login.spec.ts
@@ -0,0 +1,11 @@
+import { test, expect } from '@playwright/test';
+import {HomePage} from "../pages/home-page";
+import { LoginPage } from '../pages/login-page';
+
+test('User logs in to the MyAccount page', async ({page }) => {
+
+});
+
+test('User logs in to the MyAccount page with the Page Object Model', async ({page }) => {
+
+});
